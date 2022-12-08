ff95201c4516e5514c2bc849b27f74f1116bee43
MEGA-COOKIE-MONSTER <120087497+MEGA-COOKIE-MONSTER@users.noreply.github.com>
2022-12-08 19:43:03 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) {
+    background("blue");
+    box.x -= 5
+  }
+ 
+  if (keyIsDown(UP_ARROW)) {
+    background("yellow");
+    box.y -= 5
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    background("green");
+    box.y += 5
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    background("red");
+    box.x += 5
+  }
+  
+  drawSprites();
+}
+
